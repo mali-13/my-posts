b2f4b93d22f822b5c9df6fbc6199f260fa9ba186
mali <hello@xhemaj.dev>
2024-04-30 14:11:43 +0200

    feat: Add post dtos

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreatePostDto } from './create-post.dto';
+import { IsEnum, IsNumber, IsUrl } from 'class-validator';
+import { Status } from '../entities/post.entity';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+  @IsNumber()
+  postId: number;
+
+  @IsUrl()
+  image: string;
+
+  @IsEnum(Status)
+  status: Status;
+}
