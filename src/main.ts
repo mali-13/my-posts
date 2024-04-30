96c06bf6ea6e4b4de081c8be586c8758ec3c3306
mali <hello@xhemaj.dev>
2024-04-30 13:36:47 +0200

    feat: Add initial swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('My Posts')
+    .setDescription('My Posts API Documentation')
+    .setVersion('1.0')
+    .addTag('posts')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
